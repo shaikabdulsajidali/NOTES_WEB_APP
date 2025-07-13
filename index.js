3395eed8d3593816cf9890074156c97b31d66b21
shaikabdulsajidali <shaikabdulsajidali2909@gmail.com>
2025-07-13 18:55:25 +0530

    Task creation and Passing the filename from the backend to the frontend

--- index.js
+++ index.js
@@ -10,12 +10,17 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname,'public')))
 app.set('view engine','ejs')
 
-app.get("/",(req,res)=>{
-    console.log("hi");
+app.get("/",(req,res)=> {
     fs.readdir(`./files`,(err,filelist)=>{               //reading files from 'files' folder, 'err' will contain an error if reading the folder fails,filelist is list of files in 'files' folder
-        res.render("index",{ filesarray:filelist})
+        res.render("index",{ filesarray: filelist})
     })
     
+})
+
+app.post("/create",(req,res)=>{
+    fs.writeFile(`./files/${req.body.title.split(" ").join("")}.txt`, req.body.details,(err)=>{
+        res.redirect("/")
+    })
 })
 
 app.listen(3000,()=>{
